c6ed03500583e77ccd9b178847107bc5a8093a3c
Cao Phước Danh <7863192+caophuocdanh@users.noreply.github.com>
2025-10-04 15:30:17 +0700

    update bat file

--- build.cmd
+++ build.cmd
@@ -1,4 +1,5 @@
-pip install PySide6 pygame
-pyinstaller --onefile --windowed --icon="luckydraw.ico" --hidden-import "win32timezone" --name="Lucky Draw" main.py
+rmdir /s /q build,dist
+pip install -r requirements.txt
+pyinstaller --windowed --icon="luckydraw.ico" --hidden-import "win32timezone" --name="Lucky Draw" main.py
 xcopy assets dist\assets /E /I /Y
 
