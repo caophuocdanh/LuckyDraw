29b1337f67648da2b885e68250be2215d2327f7e
Cao Phước Danh <7863192+caophuocdanh@users.noreply.github.com>
2025-10-04 15:31:24 +0700

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 rmdir /s /q build,dist
 pip install -r requirements.txt
-pyinstaller --windowed --icon="luckydraw.ico" --hidden-import "win32timezone" --name="Lucky Draw" main.py
+pyinstaller --onefile --windowed --icon="luckydraw.ico" --hidden-import "win32timezone" --name="Lucky Draw" main.py
 xcopy assets dist\assets /E /I /Y
 
